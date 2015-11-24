b6b6238ce4fabe340fe9762fe40f2a858876a979
ywbshiwo <785534746@qq.com>
2015-11-24 19:32:31 +0800

    demo update

--- react-withsemantic/build/index.js
+++ react-withsemantic/build/index.js
@@ -11,11 +11,45 @@
 // You can think of these as markers or pieces of data that React knows how to handle.
 // React is safe.
 // We are not generating HTML strings so XSS protection is the default.
+
 "use strict";
 
 var CommentBox = React.createClass({
     displayName: "CommentBox",
 
+    getInitialState: function getInitialState() {
+        return {
+            data: []
+        };
+    },
+    loadCommentFromServer: function loadCommentFromServer() {
+        $.getJSON(this.props.url).done((function (data) {
+            this.setState({
+                data: data
+            });
+        }).bind(this)).fail(function () {});
+    },
+    handleCommentSubmit: function handleCommentSubmit(comment) {
+        //todo submit to the server and refresh the lis
+        $.ajax({
+            url: this.props.url,
+            dataType: "json",
+            type: "post",
+            data: comment,
+            success: (function () {}).bind(this),
+            error: (function () {
+                var data = this.state.data.concat([comment]);
+                this.setState({
+                    data: data
+                });
+            }).bind(this)
+        });
+    },
+    //真实DOM被插入后执行
+    componentDidMount: function componentDidMount() {
+        this.loadCommentFromServer();
+        //setInterval(this.loadCommentFromServer,this.props.pollInterval);
+    },
     render: function render() {
         return React.createElement(
             "div",
@@ -25,8 +59,8 @@ var CommentBox = React.createClass({
                 null,
                 "Comments"
             ),
-            React.createElement(CommentList, null),
-            React.createElement(CommentForm, null)
+            React.createElement(CommentList, { data: this.state.data }),
+            React.createElement(CommentForm, { onCommentSubmit: this.handleCommentSubmit })
         );
     }
 });
@@ -35,19 +69,36 @@ var CommentList = React.createClass({
     displayName: "CommentList",
 
     render: function render() {
+        var commentNodes = this.props.data.map(function (comment) {
+            return React.createElement(
+                CommentListItem,
+                { author: comment.author, key: comment.id },
+                comment.text
+            );
+        });
+
         return React.createElement(
             "div",
             { className: "commentList" },
-            "Hello, world! I am a CommentList.",
-            React.createElement(CommentListItem, null),
-            React.createElement(CommentListItem, null)
+            commentNodes
         );
     }
 });
 
+//This is a special API that intentionally makes it difficult to insert raw HTML,
+// but for marked we'll take advantage of this backdoor.
+
+//Remember: by using this feature you're relying on marked to be secure.
+// In this case, we pass sanitize: true which tells marked to
+// escape any HTML markup in the source instead of passing it through unchanged.
 var CommentListItem = React.createClass({
     displayName: "CommentListItem",
 
+    rawMarkup: function rawMarkup() {
+        var rawMarkup = marked(this.props.children.toString(), { snaitize: true });
+
+        return { __html: rawMarkup };
+    },
     render: function render() {
         return React.createElement(
             "div",
@@ -57,19 +108,58 @@ var CommentListItem = React.createClass({
                 { className: "commentAuthor" },
                 this.props.author
             ),
-            this.props.children
+            React.createElement("span", { dangerouslySetInnerHTML: this.rawMarkup() })
         );
     }
 });
 
+//注意事件和取值的写法
 var CommentForm = React.createClass({
     displayName: "CommentForm",
 
+    getInitialState: function getInitialState() {
+        return { author: '', text: '' };
+    },
+    handleAuthorChange: function handleAuthorChange(e) {
+        this.setState({ author: e.target.value });
+    },
+    handleTextChange: function handleTextChange(e) {
+        this.setState({ text: e.target.value });
+    },
+    handleSubmit: function handleSubmit(e) {
+        e.preventDefault();
+        var author = this.state.author.trim();
+        var text = this.state.text.trim();
+
+        if (!author || !text) {
+            return;
+        }
+        // send data to server
+        this.props.onCommentSubmit({
+            author: author,
+            text: text
+        });
+        this.setState({
+            author: '',
+            text: ''
+        });
+    },
     render: function render() {
+        console.log("222");
         return React.createElement(
-            "div",
-            { className: "commentForm" },
-            " Hello, world! I am a CommentFrom."
+            "form",
+            { className: "commentForm", onSubmit: this.handleSubmit },
+            React.createElement(
+                "div",
+                null,
+                React.createElement("input", { type: "text", value: this.state.author, placeholder: "Your name", onChange: this.handleAuthorChange })
+            ),
+            React.createElement(
+                "div",
+                null,
+                React.createElement("input", { type: "text", value: this.state.text, placeholder: "Say something", onChange: this.handleTextChange })
+            ),
+            React.createElement("input", { type: "submit", value: "post" })
         );
     }
 });
@@ -82,4 +172,4 @@ var CommentForm = React.createClass({
 // while React has the core tools shared by React on different platforms
 ReactDOM.render(
 //<CommentBox></CommentBox>
-React.createElement(CommentBox, null), document.getElementById("content"));
+React.createElement(CommentBox, { url: "/api/data.json", pollInterval: 2000 }), document.getElementById("content"));
--- react-withsemantic/src/index.js
+++ react-withsemantic/src/index.js
@@ -11,13 +11,53 @@
 // You can think of these as markers or pieces of data that React knows how to handle.
 // React is safe.
 // We are not generating HTML strings so XSS protection is the default.
+
+
 var CommentBox = React.createClass({
+    getInitialState: function () {
+        return {
+            data:[]
+        };
+    },
+    loadCommentFromServer: function () {
+        $.getJSON(this.props.url)
+            .done(function (data) {
+                this.setState({
+                    data:data
+                });
+            }.bind(this)).fail(function () {
+
+            });
+    },
+    handleCommentSubmit: function (comment) {
+        //todo submit to the server and refresh the lis
+        $.ajax({
+            url:this.props.url,
+            dataType:"json",
+            type:"post",
+            data:comment,
+            success: function () {
+
+            }.bind(this),
+            error: function () {
+                var data = this.state.data.concat([comment]);
+                this.setState({
+                    data:data
+                });
+            }.bind(this)
+        })
+    },
+    //真实DOM被插入后执行
+    componentDidMount: function () {
+        this.loadCommentFromServer();
+        //setInterval(this.loadCommentFromServer,this.props.pollInterval);
+    },
     render: function () {
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList />
-                <CommentForm />
+                <CommentList data={this.state.data} />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
             </div>
         );
     }
@@ -25,31 +65,79 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
     render: function () {
+        var commentNodes = this.props.data.map(function (comment) {
+            return (
+                <CommentListItem author={comment.author} key={comment.id}>{comment.text}</CommentListItem>
+            );
+        });
+
         return (
             <div className="commentList">
-                Hello, world! I am a CommentList.
-                <CommentListItem />
-                <CommentListItem />
+                {commentNodes}
             </div>
         );
     }
 });
 
+//This is a special API that intentionally makes it difficult to insert raw HTML,
+// but for marked we'll take advantage of this backdoor.
+
+//Remember: by using this feature you're relying on marked to be secure.
+// In this case, we pass sanitize: true which tells marked to
+// escape any HTML markup in the source instead of passing it through unchanged.
 var CommentListItem = React.createClass({
+    rawMarkup: function () {
+        var rawMarkup = marked(this.props.children.toString(),{ snaitize:true});
+
+        return {__html:rawMarkup};
+    },
     render: function () {
         return (
             <div className="comment">
                 <h2 className="commentAuthor">{this.props.author}</h2>
-                {this.props.children}
+                <span dangerouslySetInnerHTML={this.rawMarkup()}></span>
             </div>
         );
     }
 });
 
+//注意事件和取值的写法
 var CommentForm = React.createClass({
+    getInitialState: function () {
+        return {author:'',text:''};
+    },
+    handleAuthorChange: function (e) {
+      this.setState({author:e.target.value});
+    },
+    handleTextChange:function(e){
+        this.setState({text:e.target.value});
+    },
+    handleSubmit: function (e) {
+        e.preventDefault();
+        var author = this.state.author.trim();
+        var text = this.state.text.trim();
+
+        if(!author||!text){
+            return ;
+        }
+        // send data to server
+        this.props.onCommentSubmit({
+            author:author,
+            text:text
+        });
+        this.setState({
+            author:'',
+            text:''
+        });
+    },
     render: function () {
+        console.log("222");
         return (
-            <div className="commentForm"> Hello, world! I am a CommentFrom.</div>
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+                <div><input type="text" value={this.state.author} placeholder="Your name" onChange={this.handleAuthorChange}  /></div>
+                <div><input type="text" value={this.state.text} placeholder="Say something" onChange={this.handleTextChange} /></div>
+                <input type="submit" value="post" />
+            </form>
         )
     }
 });
@@ -62,7 +150,7 @@ var CommentForm = React.createClass({
 // while React has the core tools shared by React on different platforms
 ReactDOM.render(
     //<CommentBox></CommentBox>
-    <CommentBox />,
+    <CommentBox url ="/api/data.json" pollInterval={2000} />,
     document.getElementById("content")
 );
 
